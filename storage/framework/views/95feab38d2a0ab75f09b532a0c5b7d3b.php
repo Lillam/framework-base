288371e873f475d44c694d8ea4bfa2402896f994
Liam Taylor <liamtaylor@LTM1MBP.local>
2023-01-31 13:19:33 +0000

    fix: removal of storage and addition to gitignore for storage directories that aren't pertinent to the commits

--- storage/framework/views/95feab38d2a0ab75f09b532a0c5b7d3b.php
+++
@@ -1,7 +0,0 @@
-<?php $this->extends('layouts/main'); ?>
-@section("body")
-    Hello there
-    <?php if (1 === 1): ?>
-        <p>1 does equate to 1? surprising i know</p>
-    <?php endif; ?>
-@endsection
