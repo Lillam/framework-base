8b30e1de3111707c1b0f713dd4731ae232e155e5
Lillam <liam.taylor@outlook.com>
2025-03-13 19:56:53 +0000

    fix: removal of some unnecessary spaces in the console kernel.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -8,12 +8,12 @@ use Vyui\Foundation\Console\Kernel as ConsoleKernel;
 class Kernel extends ConsoleKernel
 {
     /**
-     * Add your own commands on boot... 
+     * Add your own commands on boot...
      *
      * @return void
      */
     public function onBoot(): void
     {
-        $this->addCommand('dict', Dict::class);        
+        $this->addCommand('dict', Dict::class);
     }
 }
